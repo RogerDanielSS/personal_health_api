7bb3df192dd753ca98698d12a8e5bf4c835fb68d
rogerdanielss <danielsimoesp@outlook.com>
2025-01-21 23:14:29 -0300

    update: add devise to manage login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,20 +17,21 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
+    unless @user.save
+      return render json: @user.errors, status: :unprocessable_entity
     end
+
+    render :show, status: :ok
   end
 
   # PATCH/PUT /users/1
   def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
+    unless @user.update(user_params) 
+      return render json: @user.errors, status: :unprocessable_entity
     end
+
+
+    render :show, status: :ok
   end
 
   private
@@ -41,7 +42,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      binding.pry
-      params.require(:user).permit(:name)
+      params.require(:user).permit(:name, :email, :password)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, skip: [:registrations]
+
+
   resources :users
   resources :items
   resources :dynamic_fields
   resources :categories
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
